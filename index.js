9e83d557a42c536d9259afd2f2ae1e7ca12addca
bony075 <bony.075@gmail.com>
2022-11-10 16:29:07 +0600

    implement jwt token to check 401 and 403 status

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
@@ -14,12 +15,42 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (error, decoded) {
+    if (error) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
+
 async function run() {
   try {
     const serviceCollection = client
       .db("cleaningService")
       .collection("services");
     const reviewCollection = client.db("cleaningService").collection("review");
+
+//jwt
+      app.post("/jwt", (req, res) => {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+          expiresIn: "1h",
+        });
+        res.send({ token });
+      });  
+
+
+
 
     app.get("/services", async (req, res) => {
       const query = {};
@@ -41,7 +72,15 @@ async function run() {
       res.send(service);
     });
 
-    app.get("/review", async (req, res) => {
+    app.get("/review",verifyJWT, async (req, res) => {
+
+      const decoded = req.decoded;
+
+      if (decoded.email !== req.query.email) {
+        res.status(403).send({ message: "unauthorized access" });
+      }
+
+
       let query = {};
       if (req.query.email) {
         query = {
@@ -53,14 +92,14 @@ async function run() {
       res.send(review);
     });
 
-    app.post("/review", async (req, res) => {
+    app.post("/review",verifyJWT, async (req, res) => {
       const review = req.body;
       const result = await reviewCollection.insertOne(review);
       res.send(result);
     });
     //delete
 
-    app.delete("/review/:id", async (req, res) => {
+    app.delete("/review/:id",verifyJWT, async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
       const result = await reviewCollection.deleteOne(query);
